ae5630783239b1e54783966c2403ee40801f4fa5
Greg Krabach <gkrabach@hartinc.com>
2017-02-08 15:55:39 -0500

    header & sidebar formatting.

--- wp-content/themes/sage/functions.php
+++ wp-content/themes/sage/functions.php
@@ -25,3 +25,10 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+// Changing excerpt more
+function new_excerpt_more($more) {
+  global $post;
+  return '… <a href="'. get_permalink($post->ID) . '">' . 'Read More &raquo;' . '</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
--- wp-content/themes/sage/templates/header.php
+++ wp-content/themes/sage/templates/header.php
@@ -1,6 +1,9 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/gs_servicemark.png" alt="" width="" height="" /></a>
+    <div class="title">
+      Troop&nbsp;75208 Temperance,&nbsp;MI
+    </div>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
