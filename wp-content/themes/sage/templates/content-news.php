0f095a493e52446e4c97d8d9cb9b877c4b539b47
Greg Krabach <gkrabach@hartinc.com>
2017-02-10 08:20:45 -0500

    redesign rank indications for news posts

--- wp-content/themes/sage/templates/content-news.php
+++ wp-content/themes/sage/templates/content-news.php
@@ -1,23 +1,4 @@
-<?php
-  global $rankParent;
-  $rankCats = wp_get_post_categories(get_the_ID(), array('parent'=>$rankParent, 'fields'=>'all'));
-  
-  switch (sizeof($rankCats)) :
-    case 0:
-      //no ranks selected, applies to the whole troop
-      $rankCats = get_categories(array('parent'=>$rankParent));
-      break;
-    case 1:
-       //only 1 rank selected, duplicate to all 4 corner;
-      $rankCats[1] = $rankCats[0];
-      $rankCats[2] = $rankCats[0];
-      $rankCats[3] = $rankCats[0];
-      break;
-    case 2://only 1 rank selected, duplicate to all 4 corner;
-      $rankCats[2] = $rankCats[0];
-      $rankCats[4] = $rankCats[1];
-      break;
-  endswitch;
+    <?php /*<?php
 ?>
 <article <?php post_class(); ?>>
   <div class="contentFrame frameCnt<?php echo sizeof($rankCats); ?>">
@@ -40,4 +21,32 @@
     </div>
   </div>
   
+</article>
+*/?>
+
+<?php
+  global $rankParent;
+  $rankCats = wp_get_post_categories(get_the_ID(), array('parent'=>$rankParent, 'fields'=>'all'));
+  
+  if (0 === sizeof($rankCats)) :
+    //no ranks selected, applies to the whole troop
+    $rankCats = get_categories(array('parent'=>$rankParent));
+  endif;
+?>
+<article <?php post_class(); ?>>
+  <div class="ranksApplied">
+    <?php foreach ($rankCats as $rc) : ?>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/<?php echo $rc->slug; ?>Tag.png" />
+    <?php endforeach; ?>
+  </div>
+  <div class="contentBlock">
+    <header>
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div>
+  </div>
+  <div style="clear: both"></div>
 </article>
